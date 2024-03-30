15ae94e10c1e2643ee5c8046e17e6bf59df2d3f4
Josa Pratama <pratamajosa1234@gmail.com>
2024-03-30 12:42:22 +0700

    add feature add, delete and search

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const contacts = [
+let contacts = [
   {
     id: 1,
     fullName: "Josa Pratama",
     email: "josa@gmail.com",
     phoneNumber: "+6281293847592",
     label: ["owner"],
-    birthday: "09/09/2006",
+    birthDay: "09/09/2006",
     avatar: null,
   },
   {
@@ -14,7 +14,7 @@ const contacts = [
     email: "rafli@gmail.com",
     phoneNumber: "+6282192740493",
     label: ["friend"],
-    birthday: "20/08/2008",
+    birthDay: "20/08/2008",
     avatar: null,
   },
   {
@@ -23,20 +23,38 @@ const contacts = [
     email: "risky@gmail.com",
     phoneNumber: "+62839373829283",
     label: ["friend"],
-    birthday: "20/08/2008",
+    birthDay: "20/08/2008",
     avatar: null,
   },
 ];
 
+const searchInputElement = document.getElementById("search-input");
+const addContactFormElement = document.getElementById("add-contact-form");
 const contactsContainerElement = document.getElementById("contacts-container");
 
 function renderContacts() {
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  const keyword = params.get("q");
+
+  if (keyword) {
+    searchInputElement.value = keyword;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.fullName.toLowerCase().includes(keyword.toLowerCase())
+    );
+
+    contacts = filteredContacts;
+  }
+
   const contactItemElements = contacts.map(
     (contact) => `
 <li>
   <h2>${contact.fullName}</h2>
   <p>${contact.email}</p>
   <p>${contact.phoneNumber}</p>
+  <p>${contact.birthDay}</p>
+  <div><button onclick="deleteContactById(${contact.id})">Delete</button></div>
 </li>
 `
   );
@@ -46,16 +64,32 @@ function renderContacts() {
   contactsContainerElement.innerHTML = contactItems;
 }
 
-function addContact(fullName, email, phoneNumber) {
+function addContact(event) {
+  event.preventDefault();
   const lastId = contacts[contacts.length - 1].id;
-  contact.push({ id: lastId + 1, fullName, email, phoneNumber });
+  const contactFormData = new FormData(addContactFormElement);
+  const newContact = {
+    id: lastId + 1,
+    fullName: contactFormData.get("fullName"),
+    email: contactFormData.get("email"),
+    phoneNumber: contactFormData.get("phoneNumber"),
+    birthDay: contactFormData.get("birthDay"),
+  };
+  contacts.push(newContact);
   renderContacts();
 }
 
-function deleteContactById(id) {}
+function deleteContactById(id) {
+  const updatedContacts = contacts.filter(
+    (contact) => contact.id !== Number(id)
+  );
+  console.log(updatedContacts);
+  contacts = updatedContacts;
+  renderContacts();
+}
 
 function updateContactById(id) {}
 
-function searchContact(keyword) {}
+window.addEventListener("load", renderContacts);
 
-renderContacts();
+addContactFormElement.addEventListener("submit", addContact);
